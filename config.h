2b1bb0e7fd1e6496ff017d8a2ab192718e52ee71
Piotr Kira <piotrakira@gmail.com>
2021-04-17 22:08:21 +0200

    Add mpd hotkeys

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx	    = 6;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "Hack:size=12", "Noto Color Emoji:size=10" };
+static const char dmenufont[]       = "Hack:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#9e9e9e";
 static const char col_gray3[]       = "#bbbbbb";
@@ -61,20 +61,24 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_bar, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *watch_int_mpv[] = { "/home/pkira/dotfiles/scripts/watch-in-mpv.sh", NULL };
+static const char *slock[] = { "slock", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_v,      spawn,          {.v = watch_int_mpv } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd} },
+	{ MODKEY,                       XK_s,      spawn,          {.v = slock} },
+	{ 0,                       XK_Print,      spawn,          SHCMD("maim -i $(xdotool getactivewindow) ~/media/ss/ss$(date +%Y%m%d%H%M_%3N).png") },
+	{ MODKEY,                       XK_Print,      spawn,          SHCMD("maim -s ~/media/ss/ss$(date +%Y%m%d%H%M_%3N).png") },
+	//{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
@@ -106,6 +110,9 @@ static Key keys[] = {
 	{ 0, XF86XK_AudioMute,             spawn,           SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)")},
 	{ 0, XF86XK_AudioRaiseVolume,      spawn,           SHCMD("pamixer -i 3; kill -44 $(pidof dwmblocks)")},
 	{ 0, XF86XK_AudioLowerVolume,      spawn,           SHCMD("pamixer -d 3; kill -44 $(pidof dwmblocks)")},
+	{ 0, XF86XK_AudioPrev,      spawn,           SHCMD("mpc prev")},
+	{ 0, XF86XK_AudioNext,      spawn,           SHCMD("mpc next")},
+	{ 0, XF86XK_AudioPlay,      spawn,           SHCMD("mpc toggle")},
 };
 
 /* button definitions */
